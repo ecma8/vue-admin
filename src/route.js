24cdaaa24552e4bce48933dd9d0407050453b22e
ecma8 <756932563@qq.com>
2017-09-22 19:47:33 +0800

    router

--- src/route.js
+++ src/route.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 const Vuex = resolve => require(['./components/Vuex.vue'], resolve);
 
 const Home = resolve => require(['./components/Home.vue'], resolve);
